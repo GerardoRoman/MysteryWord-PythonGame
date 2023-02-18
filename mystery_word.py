92b5f5ad865b26198e08661a565c1bb002f4f738
GerardoRoman <offthegrd3657@gmail.com>
2023-02-18 15:35:42 -0500

    opened words.txt file

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,37 @@
-def play_game():
-    pass
+import random
 
 
+def open_file(file):
+    with open(file) as game_file:
+        data = game_file.read()
+        format_data = data.split()
+        return format_data
+
+print(open_file('words.txt'))
+
+def choose_random_word(list):
+    word = random.choice(list)
+    return word
+
+# def play_game():
+
+
+# EXAMPLE FROM CLASS::
+# def play_game():
+#     word = 'apple'
+#     wrong_guesses = ['b', 'd']
+#     right_guesses = []
+    
+#     game_board = ''
+#     for letter in word:
+#         game_board += ' '
+#         if letter in right_guesses:
+#             game_board += letter + ' '
+#         else:
+#             game_board += "_ "
+#     print(game_board)
+
+
+# do not change code below this line
 if __name__ == "__main__":
     play_game()
