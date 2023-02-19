2d3c5daa5d8e02c54b8c3734ac3296f44b5b0ccb
GerardoRoman <offthegrd3657@gmail.com>
2023-02-18 19:20:25 -0500

    got code to open words.txt file and randomly generate words with blanks

--- mystery_word.py
+++ mystery_word.py
@@ -5,7 +5,10 @@ def open_file(file):
     with open(file) as game_file:
         data = game_file.read()
         format_data = data.split()
-        return format_data
+        random_word = random.choice(format_data)
+        random_word_list = list(random_word)
+        blanks = [' _ '] * len(random_word)
+    return random_word_list, blanks
 
 print(open_file('words.txt'))
 
